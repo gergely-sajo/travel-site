68bbcf718faa83ebbb637ad17846caedc9b54fee
sajogergo <sajo.gergo@gmail.com>
2020-06-04 08:29:01 +0200

    Updated cloud function to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,25 @@
 exports.handler = function (event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome"
-    })
+    const secretContent = `
+    <h3>Welcome</h3>
+    <p>The password you entered were correct.</p>
+    `
+    
+    let body
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+    if (body.password == "password") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    } else {
+        callback(null, {
+            statusCode: 401
+        })
+    }
 }
